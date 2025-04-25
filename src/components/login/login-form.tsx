c60d125c58764284e1338e74b0fac7c42221f24f
Gabrielneves05 <gabrielneves05@outlook.com>
2025-04-25 13:25:10 -0300

    feat: creating a login form / capturing user data in the api

---
+++ src/components/login/login-form.tsx
@@ -0,0 +1,22 @@
+import login from "@/actions/login";
+
+export default function LoginForm() {
+    return (
+        <>
+            <form action={login}>
+                <input 
+                    type="text" 
+                    name="username" 
+                    placeholder="Usuário" 
+                />
+                <input 
+                    type="password" 
+                    name="password"
+                    placeholder="Senha" 
+                />
+
+                <button type="submit">Entrar</button>
+            </form>
+        </>
+    );
+}
